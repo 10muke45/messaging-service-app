64e7f169b989a99dbfbd16f10694f11dbf1b3444
10muke45 <mukeshdhandapani360s@gmail.com>
2024-10-22 20:42:57 +0530

    secure ws

--- app.js
+++ app.js
@@ -108,8 +108,8 @@ document.addEventListener("DOMContentLoaded", function() {
             return;
         }
 
-        const ws = new WebSocket(`ws://tjhn8rgw-8080.inc1.devtunnels.ms/ws?token=${token}`);
-        
+        const ws = new WebSocket(`wss://tjhn8rgw-8080.inc1.devtunnels.ms/ws?token=${token}`);
+
 
         ws.onopen = function() {
             console.log("WebSocket connection established.");
